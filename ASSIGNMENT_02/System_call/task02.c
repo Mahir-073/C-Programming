196ef37f2275162247ce29219b2f42467d5fce89
MAHIR AHMED NILOY <137860201+Mahir-073@users.noreply.github.com>
2023-08-26 19:29:26 +0600

    Sheets Uploaded

--- ASSIGNMENT_02/System_call/task02.c
+++ ASSIGNMENT_02/System_call/task02.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/wait.h>
 
 
-int main() {
-    // create child process
-    pid_t child_pid = fork();
+// int main() {
+//     // create child process
+//     pid_t child_pid = fork();
 
-    if (child_pid == 0) {
-        // child process
-        pid_t grandchild_pid = fork();
+//     if (child_pid == 0) {
+//         // child process
+//         pid_t grandchild_pid = fork();
 
-        if (grandchild_pid == 0) {
-            // grandchild process
-            printf("I am grandchild\n");
-        } else {
-            // child process
-            wait(NULL);
-            printf("I am child\n");
-        }
-    } else {
-        // parent process
-        wait(NULL);
-        printf("I am parent\n");
-    }
+//         if (grandchild_pid == 0) {
+//             // grandchild process
+//             printf("I am grandchild\n");
+//         } else {
+//             // child process
+//             wait(NULL);
+//             printf("I am child\n");
+//         }
+//     } else {
+//         // parent process
+//         wait(NULL);
+//         printf("I am parent\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
